8941c007e097ed3095e02e9d30d60ff19053520d
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2024-08-06 13:19:26 -0700

    renamed colors, added cards to list

---
+++ client/src/components/boards/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+        <div className="bg-primary-blue-1">
+            <p>CARD NAME</p>
+        </div>
+    );
+}
--- client/src/components/boards/List.jsx
+++ client/src/components/boards/List.jsx
@@ -1,13 +1,13 @@
+import Card from "./Card";
+
 export default function List() {
     return (
-        <div className="border rounded-xl w-fit">
+        <div className="border rounded-xl w-fit p-4 bg-primary-blue-2">
             <div className="flex">
-                <p className="mr-6">LIST NAME</p>
+                <p className="mr-6 text-off-white">LIST NAME</p>
                 <button>OPTIONS</button>
             </div>
-            <div>
-                <p>CARD NAME</p>
-            </div>
+            <Card />
         </div>
     );
 }
