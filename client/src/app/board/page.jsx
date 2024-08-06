6e8f236109cfcca0f1607ae63aee855d36ac364b
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2024-08-06 12:56:50 -0700

    Created a basic list

--- client/src/app/board/page.jsx
+++ client/src/app/board/page.jsx
@@ -1,14 +1,19 @@
+import List from "../../components/boards/List";
+
 export const metadata = {
     title: "Boards Page",
 }
 
 export default function Page() {
     return (
-        <div>
-            <div className="flex place-content-between">
-                <h1>BOARD NAME</h1>
-                <button>OPTIONS</button>
+        <>
+            <div>
+                <div className="flex place-content-between">
+                    <h1>BOARD NAME</h1>
+                    <button>OPTIONS</button>
+                </div>
             </div>
-        </div>
+            <List />
+        </>
     );
 }
---
+++ client/src/components/boards/List.jsx
@@ -0,0 +1,13 @@
+export default function List() {
+    return (
+        <div className="border rounded-xl w-fit">
+            <div className="flex">
+                <p className="mr-6">LIST NAME</p>
+                <button>OPTIONS</button>
+            </div>
+            <div>
+                <p>CARD NAME</p>
+            </div>
+        </div>
+    );
+}
