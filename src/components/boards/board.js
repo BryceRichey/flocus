4166b0534f3d04c5dda7c0c1b8a3f90bf9f80076
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2024-08-29 10:48:24 -0700

    added button to add a new list

--- src/components/boards/board.js
+++ src/components/boards/board.js
@@ -1,4 +1,5 @@
 import List from "@/components/lists/list";
+import NewListForm from "./new-list-form";
 
 export default function Board({ board }) {
     if (board === null) {
@@ -13,14 +14,17 @@ export default function Board({ board }) {
                 <h1 className="text-xl">{board.boardName}</h1>
             </div>
             <br />
-            <ol className="flex">
-                {board.lists.map((list) => (
-                    <li key={list.id} className="rounded bg-stone-400 mx-2 p-4">
-                        <h2 className="mb-4 text-white">{list.listName}</h2>
-                        <List listCards={list.cards} />
-                    </li>
-                ))}
-            </ol>
+            <div className="flex">
+                <ol className="flex">
+                    {board.lists.map((list) => (
+                        <li key={list.id} className="rounded bg-stone-400 mx-2 p-4">
+                            <h2 className="mb-4 text-white">{list.listName}</h2>
+                            <List listCards={list.cards} />
+                        </li>
+                    ))}
+                </ol>
+                <NewListForm />
+            </div>
         </main >
     );
 }
--- src/components/boards/new-list-form.js
+++ src/components/boards/new-list-form.js
@@ -1,6 +1,7 @@
 export default function NewListForm() {
     return (
         <div>
+            <button type="button" className="bg-stone-600 text-white rounded px-2 py-1">Add a new list</button>
             <form action="">
                 <input type="text" />
             </form>
