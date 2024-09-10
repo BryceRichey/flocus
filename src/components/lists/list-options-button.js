24a808f46e751e5afa182c0365f780f4ec363257
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2024-09-10 13:32:48 -0700

    added icons folder and more icon

--- src/components/lists/list-options-button.js
+++ src/components/lists/list-options-button.js
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import DeleteListForm from "./delete-list-form";
+import MoreIcon from "../icons/more";
 
 export default function ListOptionsButton({ boardId, listId }) {
     const [showForm, setShowForm] = useState(false);
@@ -16,7 +17,9 @@ export default function ListOptionsButton({ boardId, listId }) {
     return (
         <>
             <div>
-                <button type="button" className="bg-stone-600 hover:bg-stone-500 text-white text-xs rounded-lg h-6 px-2" onClick={handleClick}>Options</button>
+                <button type="button" className="bg-stone-600 hover:bg-stone-500 text-white text-xs rounded-lg h-" onClick={handleClick}>
+                    <MoreIcon />
+                </button>
                 <section className={`${showForm ? '' : 'hidden'} absolute  bg-stone-500 p-5`}>
                     <Link href={`/boards/${boardId}/${listId}`} className="bg-stone-600 text-white rounded px-2 py-1 me-2">Edit List</Link>
                     <DeleteListForm boardId={boardId} listId={listId} />
