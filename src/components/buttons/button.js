a4544a613bb80b4f7b4c23e118daadca806eaf05
BryceRichey <107497353+BryceRichey@users.noreply.github.com>
2024-09-17 15:07:50 -0700

    updated create list button and form styles

--- src/components/buttons/button.js
+++ src/components/buttons/button.js
@@ -1,22 +1,27 @@
-export default function Button({ buttonType, buttonStyle, buttonLabel, onClickFn, iconLeft, widthFill }) {
+export default function Button({ buttonType, buttonStyle, buttonLabel, onClickFn, iconLeft, width }) {
     if (!buttonStyle) {
         console.log("buttonStyle needed");
         return null;
     }
 
-    let buttonClasses = 'font-medium text-base px-3 py-2 rounded-lg w-max';
+    let buttonClasses = 'font-medium text-base px-3 py-2 rounded-lg';
     let styleClasses = ''
+    let widthClasses = 'w-max'
 
     if (buttonStyle === 'fill') {
-        styleClasses = 'text-white bg-cyan-950 hover:bg-cyan-900';
+        styleClasses = 'text-white text-left bg-cyan-950 hover:bg-cyan-900';
     } else if (buttonStyle === 'ghost') {
-        styleClasses = 'text-white/95 hover:text-white hover:bg-cyan-900/20';
+        styleClasses = 'text-white/95 text-left hover:text-white hover:bg-cyan-900/20';
+    }
+
+    if (width) {
+        widthClasses = width;
     }
 
     return (
         <button
             type={buttonType ? buttonType : 'button'}
-            className={`${buttonClasses} ${styleClasses}`}
+            className={`${buttonClasses} ${styleClasses} ${widthClasses}`}
             onClick={onClickFn}>
             {iconLeft ? <span className="pe-2">{iconLeft}</span> : ''}
             {buttonLabel ? buttonLabel : 'Test Button'}
--- src/components/lists/new-list-form.js
+++ src/components/lists/new-list-form.js
@@ -24,18 +24,17 @@ export default function NewListForm({ boardId }) {
 
     if (!showForm) {
         content = (
-            <div>
-                <button type="button" className="bg-stone-600 hover:bg-stone-500 text-white rounded-lg mx-2 px-2 py-1 w-40 h-12" onClick={handleClick}>Create new list</button>
+            <div className='mx-2 w-64'>
+                <Button buttonType="button" buttonStyle="fill" buttonLabel="Create another list" onClickFn={handleClick} width="w-64" iconLeft="+" />
             </div>
         );
     } else {
         content = (
             <section>
-                <form action={formAction} className={`${showForm ? '' : 'hidden'} flex flex-col justify-between rounded-lg bg-stone-400 mx-2 mb-2 p-2 w-40`}>
+                <form action={formAction} className={`${showForm ? '' : 'hidden'} flex flex-col justify-between rounded-lg bg-cyan-600 mx-2 mb-2 p-2 w-64`}>
                     <input type="hidden" name="boardId" value={boardId} />
-                    <label className='text-white'>List Name</label>
-                    <input type="text" name="listName" className='rounded-lg mb-2' />
-                    <div className='flex justify-between'>
+                    <input type="text" name="listName" className='rounded-lg mb-2 p-2' placeholder='Enter a name for this list' />
+                    <div className='flex'>
                         <Button buttonType="button" buttonStyle="ghost" buttonLabel="Cancel" onClickFn={handleClick} />
                         <Button buttonType="submit" buttonStyle="fill" buttonLabel="Create" />
                     </div>
